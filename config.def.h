bcca64c9ba61e873f5ef57791bd81c34b5c65e9d
Abel D. <mail@abel.fyi>
2026-08-18 00:52:44 +0200

    wire up volume and brightness keys
    XF86 keys -> amixer / brightnessctl
    
    (cherry picked from commit 4bb95663bd1d31a269b5c4848485b32626c5f24c)
    (cherry picked from commit 69a44abc3f655f6639aff941833d41ae78993ebd)

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -121,6 +122,11 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_plus,   setgaps,        {.i = +1 } },
+	{ 0,           XF86XK_AudioRaiseVolume,   spawn, SHCMD("amixer -q sset Master 5%+ unmute") },
+	{ 0,           XF86XK_AudioLowerVolume,   spawn, SHCMD("amixer -q sset Master 5%- unmute") },
+	{ 0,           XF86XK_AudioMute,          spawn, SHCMD("amixer -q sset Master toggle") },
+	{ 0,           XF86XK_MonBrightnessUp,    spawn, SHCMD("brightnessctl set 5%+") },
+	{ 0,           XF86XK_MonBrightnessDown,  spawn, SHCMD("brightnessctl set 5%-") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
